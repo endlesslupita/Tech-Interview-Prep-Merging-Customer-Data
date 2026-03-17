0e19217e236a297b26d4fbe0f3ba8bc7ffa87945
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-16 18:40:08 -0700

    Start test cases

---
+++ test_solution.py
@@ -0,0 +1,37 @@
+from solution import merge_data
+
+def test_normal_case_1():
+        assert merge_data([]) == []
+
+def test_normal_case_2():
+        assert merge_data([]) == []
+
+def test_normal_case_3():
+        assert merge_data([]) == []
+
+def test_edge_case_1():
+        assert merge_data([]) == []
+
+def test_edge_case_2():
+        assert merge_data([]) == []
+
+def test_edge_case_3():
+        assert merge_data([]) == []
+
+def test_normal_case_1_opt():
+        assert merge_data([]) == []
+
+def test_normal_case_2_opt():
+        assert merge_data([]) == []
+
+def test_normal_case_3_opt():
+        assert merge_data([]) == []
+
+def test_edge_case_1_opt():
+        assert merge_data([]) == []
+
+def test_edge_case_2_opt():
+        assert merge_data([]) == []
+
+def test_edge_case_3_opt():
+        assert merge_data([]) == []
