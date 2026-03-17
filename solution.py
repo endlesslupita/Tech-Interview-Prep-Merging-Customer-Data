56f55105921dc5c8f729dd8309a1bf4efefb9052
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-16 19:14:17 -0700

    Fixing bugs with Claude's help

--- solution.py
+++ solution.py
@@ -1,12 +1,13 @@
 def merge_data(customerData1, customerData2):
-    position = 0
+    position = -1
     
     while customerData2:
-        if customerData1[position]>customerData2[0]:
-            insert(customerData2[position], customerData1[0])
-            position += 1
+        position += 1
+        if customerData1[position] > customerData2[0]:
+            customerData1.insert(position,customerData2[0])
+            customerData2.pop(0)
 
     return customerData1
 
 if __name__ == '__main__':
-    print(merge_data([101,104,107,0,0,0],[102,105,108]))
+    print(merge_data([101, 104, 107, 0, 0, 0],[102, 105, 108]))
