f534e77925c630c3356a7eccc304719a45f373c6
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-16 21:40:44 -0700

    Partial changes to naive solution and beginning of optimized solution

--- solution.py
+++ solution.py
@@ -1,12 +1,20 @@
 def merge_data(customerData1, customerData2):
-    position = -1
-    
-    while customerData2:
-        position += 1
-        if customerData1[position] > customerData2[0]:
-            customerData1.insert(position,customerData2[0])
-            customerData2.pop(0)
 
+    position = 0
+    list_length = len(customerData1)
+    list_length -= 1
+    while customerData2 and position <= list_length:
+        if customerData1[position] < customerData2[0]:
+            position += 1
+        else:
+            customerData1[position] = customerData2[0]
+            customerData2.pop(0)
+            position += 1
+    position -= 1
+    while customerData2:
+            customerData1[position] = customerData2[0]
+            customerData2.pop(0)
+            position += 1
     return customerData1
 
 if __name__ == '__main__':
