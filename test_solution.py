f21f4c144924e8d42de3b11eaa41e7b43d2bd261
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-22 23:24:04 -0700

    Update tests to run on optimized solution because brute force solution never worked

--- test_solution.py
+++ test_solution.py
@@ -1,40 +1,22 @@
-from solution import merge_data
+from solution import merge_data_opt
 
 def test_normal_case_1():
-        assert merge_data([12, 40, 135, 0, 0],[1, 20]) == [1, 12, 20, 40, 135]
+        assert merge_data_opt([12, 40, 135, 0, 0],[1, 20]) == [1, 12, 20, 40, 135]
 
 def test_normal_case_2():
-        assert merge_data([235, 427, 600, 0, 0, 0],[936, 1006, 1845]) == [235, 427, 600, 936, 1006, 1845]
+        assert merge_data_opt([235, 427, 600, 0, 0, 0],[936, 1006, 1845]) == [235, 427, 600, 936, 1006, 1845]
 
 def test_normal_case_3():
-        assert merge_data([2, 9, 0],[1]) == [1, 2, 9]
+        assert merge_data_opt([2, 9, 0],[1]) == [1, 2, 9]
 
 def test_edge_case_1():
 #empty lists
-        assert merge_data([],[]) == []
+        assert merge_data_opt([],[]) == []
 
 def test_edge_case_2():
 # one value in list2
-        assert merge_data([0],[4]) == [4]
+        assert merge_data_opt([0],[4]) == [4]
 
 def test_edge_case_3():
 # one value in list1
-        assert merge_data([4],[]) == [4]
-
-# def test_normal_case_1_opt():
-#         assert merge_data([],[]) == []
-
-# def test_normal_case_2_opt():
-#         assert merge_data([],[]) == []
-
-# def test_normal_case_3_opt():
-#         assert merge_data([],[]) == []
-
-# def test_edge_case_1_opt():
-#         assert merge_data([],[]) == []
-
-# def test_edge_case_2_opt():
-#         assert merge_data([],[]) == []
-
-# def test_edge_case_3_opt():
-#         assert merge_data([],[]) == []
+        assert merge_data_opt([4],[]) == [4]
