025a4a75e6db3741390481e809b30f80a24ca25b
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-22 23:02:39 -0700

    My solution with Claude's help

--- solution.py
+++ solution.py
@@ -17,5 +17,25 @@ def merge_data(customerData1, customerData2):
             position += 1
     return customerData1
 
+
+def merge_data_opt(customerData1, customerData2):
+     
+     d1 = m - 1
+     d2 = n - 1
+     fill = (m + n) - 1
+
+     while customerData[fill] == 0:
+          fill -= 1
+
+    while d2 > 0:
+        while d1 > -1:
+            if customerData1[d1] > customerData2[d2]:
+                customerData1[fill] = customerData1[d1]
+                d1 -= 1
+            else:
+                customerData1[fill] = customerData2[d2]
+                d2 -= 1
+        customerData1[fill] = customerData2[d2]
+
 if __name__ == '__main__':
     print(merge_data([101, 104, 107, 0, 0, 0],[102, 105, 108]))
